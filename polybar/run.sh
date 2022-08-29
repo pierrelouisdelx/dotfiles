e4d7b3e1580d41b2241b2705c33780b437eef603
Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>
2022-08-29 19:25:39 +0200

    wlan interface fixed
    Signed-off-by: Pierre-Louis Delcroix <pierrelouisdelcroix2001@gmail.com>

--- polybar/run.sh
+++ polybar/run.sh
@@ -8,10 +8,11 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Get Wifi interface
 for dev in `ls /sys/class/net`; do
-    [ -d "/sys/class/net/$dev/wireless" ] && WLAN=$dev
+    [ -d "/sys/class/net/$dev/wireless" ]
+    w=$dev
 done
 
 # Launch polybar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar main --reload &
+    MONITOR=$m WLAN=$w polybar main --reload &
 done
